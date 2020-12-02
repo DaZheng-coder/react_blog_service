f31b2827607d2a6ba288a1f4a94fc79115b91e50
请不要阻拦我学习 <924370430@qq.com>
2020-12-02 20:50:30 +0800

    添加获取文章列表接口

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-02 16:23:57
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-02 20:23:48
+ * @LastEditTime: 2020-12-02 20:49:38
  * @Description: file content
  */
 
@@ -10,9 +10,20 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    let result = await this.app.mysql.get('blog_content', {})
-    console.log(result)
-    this.ctx.body = result
+    this.ctx.body = 'api hi'
+  }
+
+  // 得到文章列表
+  async getArticleList() {
+    let sql = 'SELECT article.id as id ,' +
+              'article.title as title ,' +
+              'article.introduce as introduce ,' +
+              'article.addTime as addTime ,' +
+              'article.view_count as view_count ,' +
+              'type.typeName as typeName ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.id'
+    const results = await this.app.mysql.query(sql)
+    this.ctx.body = { data:results }
   }
 }
 
--- app/router/default.js
+++ app/router/default.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-02 16:27:27
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-02 16:30:07
+ * @LastEditTime: 2020-12-02 20:48:01
  * @Description: file content
  */
 
@@ -11,4 +11,5 @@
 module.exports = app => {
   const { router, controller } = app
   router.get('/default/index', controller.default.home.index)
+  router.get('/default/getArticleList', controller.default.home.getArticleList)
 }

6b0a9126cb619eb8cea1b785aab25658b29beae0
请不要阻拦我学习 <924370430@qq.com>
2020-12-02 23:30:38 +0800

    文章详情页接口

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-02 16:23:57
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-02 20:49:38
+ * @LastEditTime: 2020-12-02 22:14:03
  * @Description: file content
  */
 
@@ -18,13 +18,30 @@ class HomeController extends Controller {
     let sql = 'SELECT article.id as id ,' +
               'article.title as title ,' +
               'article.introduce as introduce ,' +
-              'article.addTime as addTime ,' +
+              "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
               'article.view_count as view_count ,' +
               'type.typeName as typeName ' +
               'FROM article LEFT JOIN type ON article.type_id = type.id'
     const results = await this.app.mysql.query(sql)
     this.ctx.body = { data:results }
   }
+
+  async getArticleById() {
+    let id = this.ctx.params.id
+    let sql = 'SELECT article.id as id ,' +
+              'article.title as title ,' +
+              'article.introduce as introduce ,' +
+              'article.article_content as article_content ,' +
+              "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
+              'article.view_count as view_count ,' +
+              'type.typeName as typeName ' +
+              'type.id as typeId ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+              'WHERE article.id=' + id
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = {data:result}
+  }
+
 }
 
 module.exports = HomeController;
--- app/router/default.js
+++ app/router/default.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-02 16:27:27
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-02 20:48:01
+ * @LastEditTime: 2020-12-02 21:55:28
  * @Description: file content
  */
 
@@ -12,4 +12,5 @@ module.exports = app => {
   const { router, controller } = app
   router.get('/default/index', controller.default.home.index)
   router.get('/default/getArticleList', controller.default.home.getArticleList)
+  router.get('/default/getArticleById', controller.default.home.getArticleById)
 }

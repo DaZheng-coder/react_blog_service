3c50a1a9e4e10d45d68abb265e25ebc54bd7c8b6
请不要阻拦我学习 <924370430@qq.com>
2020-12-03 15:46:59 +0800

    添加根据类型请求文章列表接口

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-02 16:23:57
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-03 12:16:35
+ * @LastEditTime: 2020-12-03 15:25:19
  * @Description: file content
  */
 
@@ -46,6 +46,23 @@ class HomeController extends Controller {
   async getTypeInfo () {
     const result = await this.app.mysql.select('type')
     this.ctx.body = { data: result }
+  }
+
+  // 根据类别id获得文章列表
+  async getListById () {
+    let id = this.ctx.params.id
+    let sql = 'SELECT article.id as id ,' +
+              'article.title as title ,' +
+              'article.introduce as introduce ,' +
+              'article.article_content as article_content ,' +
+              "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
+              'article.view_count as view_count ,' +
+              'type.typeName as typeName ,' +
+              'type.id as typeId ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+              'WHERE type_id=' + id
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = {data:result}
   }
 
 }
